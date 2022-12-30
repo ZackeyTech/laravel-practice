8b18aa72e2b6ef0b4da4c7154b37c47af0590f4b
Zackey <yamazaki@zackey.tech>
2022-12-30 18:21:47 +0900

    check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CircleCi for Laravel form ZACKEY
                 </div>
 
                 <div class="links">
